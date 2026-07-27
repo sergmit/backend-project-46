4681269ece92bfeb8ed272917808fbcf11d0e587
Sergey <mitugov@bk.ru>
2026-07-27 12:42:57 +0300

    Fix commander

--- src/commander.js
+++ src/commander.js
@@ -4,7 +4,8 @@ const action = () => {
   program.name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
-    .arguments('<filename1> <filename2>')
+    .argument("<filepath1>")
+    .argument("<filepath2>")
     .option('-f, --format <string>', 'Format output')
     .action((filepath1, filepath2) => {
       console.log(genDiff(filepath1, filepath2, program.opts().format));
